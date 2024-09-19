9fe544ab061c17748cc74ba3d804741687021b48
yosa12978 <yosa12978@gmail.com>
2024-09-19 20:51:45 +0300

    added static file server

--- internal/router/router.go
+++ internal/router/router.go
@@ -21,6 +21,9 @@ func NewRouter(opts ...optionFunc) http.Handler {
 }
 
 func addRoutes(router *http.ServeMux, opts options) {
+	fileServer := http.FileServer(http.Dir("./static"))
+	router.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		utils.RenderTemplate(w, "index", nil)
