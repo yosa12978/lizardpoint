49ce874f7ec459343c26815623058c9ab991fd2a
yosa12978 <yosa12978@gmail.com>
2024-10-06 23:14:14 +0300

    added account create dto

--- internal/types/account.go
+++ internal/types/account.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"context"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,4 +16,23 @@ type Account struct {
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	Roles        []Role
+}
+
+type CreateAccountDto struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	//PasswordConfirm string `json:"password_confirm"`
+}
+
+func (c CreateAccountDto) Validate(ctx context.Context) (CreateAccountDto, map[string]string, bool) {
+	problems := make(map[string]string)
+	c.Username = strings.TrimSpace(c.Username)
+	c.Password = strings.TrimSpace(c.Password)
+	if len(c.Password) < 8 {
+		problems["password"] = "password length must be >= 8"
+	}
+	if c.Username == "" {
+		problems["username"] = "username can't be empty"
+	}
+	return c, problems, len(problems) == 0
 }
