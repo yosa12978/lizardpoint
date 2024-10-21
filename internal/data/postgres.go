be4fd751e2638233b4cbe099ddd6d6497396aa34
yosa12978 <yosa12978@gmail.com>
2024-10-21 11:22:27 +0300

    changed postgres connection method

--- internal/data/postgres.go
+++ internal/data/postgres.go
@@ -12,7 +12,7 @@ import (
 	"github.com/yosa12978/lizardpoint/internal/logging"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
@@ -21,61 +21,74 @@ var (
 	pgDb   *sql.DB
 )
 
-func connectPostgres(ctx context.Context, logger logging.Logger) func() error {
-	return func() error {
-		conf := config.Get()
-		url := fmt.Sprintf(
-			"postgres://%s:%s@%s/%s?sslmode=%s",
-			conf.Postgres.User,
-			conf.Postgres.Password,
-			conf.Postgres.URL,
-			conf.Postgres.Database,
-			conf.Postgres.SSLMode,
-		)
-		conn, err := sql.Open("postgres", url)
-		if err != nil {
-			logger.Error("error opening postgres connection", "error", err.Error())
-			return err
-		}
-		if err := conn.PingContext(ctx); err != nil {
-			logger.Error("error verifying postgres connection", "error", err.Error())
-			return err
-		}
-		pgDb = conn
-
-		migrator, err := migrate.New(conf.Postgres.MigrationsPath, url)
-		if err != nil {
-			logger.Error("error creating a new migrator", "error", err.Error())
-			return err
-		}
-		defer migrator.Close()
-
-		if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			if errDirty, ok := err.(migrate.ErrDirty); ok {
-				logger.Error(
-					"migration is dirty, forcing rollback and retrying",
-					"error", err.Error(),
-				)
-				migrator.Force(errDirty.Version - 1)
-				if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-					logger.Error("migration error", "error", err.Error())
-					return err
-				}
-			} else {
-				logger.Error("migration error", "error", err.Error())
-				return err
-			}
-		}
-		logger.Info("database migrations completed successfully")
-		return nil
+func newPostgresConn(ctx context.Context) (*sql.DB, error) {
+	conf := config.Get()
+	url := fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=%s",
+		conf.Postgres.User,
+		conf.Postgres.Password,
+		conf.Postgres.URL,
+		conf.Postgres.Database,
+		conf.Postgres.SSLMode,
+	)
+	conn, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
 	}
+	if err := conn.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	return conn, err
 }
 
 func Postgres(ctx context.Context, logger logging.Logger) *sql.DB {
 	pgInit.Do(func() {
-		if err := connectPostgres(ctx, logger)(); err != nil {
+		conn, err := newPostgresConn(ctx)
+		if err != nil {
+			logger.Error("error while connecting to postgres")
+			panic(err)
+		}
+		migrationsPath := config.Get().Postgres.MigrationsPath
+		if err := migrateUp(conn, migrationsPath, logger); err != nil {
+			logger.Error("error while applying migrations")
 			panic(err)
 		}
 	})
 	return pgDb
+}
+
+func migrateUp(db *sql.DB, migrations string, logger logging.Logger) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+	migrator, err := migrate.NewWithDatabaseInstance(
+		migrations,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		logger.Error("error creating a new migrator")
+		return err
+	}
+	defer migrator.Close()
+
+	if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		if errDirty, ok := err.(migrate.ErrDirty); ok {
+			logger.Error(
+				"migration is dirty, forcing rollback and retrying",
+				"error", err.Error(),
+			)
+			migrator.Force(errDirty.Version - 1)
+			if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+				logger.Error("migration error")
+				return err
+			}
+		} else {
+			logger.Error("migration error")
+			return err
+		}
+	}
+	logger.Info("database migrations completed successfully")
+	return nil
 }
